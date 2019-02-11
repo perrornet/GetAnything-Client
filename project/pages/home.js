4f4ad2e7a07d0fee580f47c646995c56fb66c687
perror <15058342792@163.com>
2019-02-12 01:44:34 +0800

    增加m3u8文件下载的支持

--- project/pages/home.js
+++ project/pages/home.js
@@ -1,3 +1,4 @@
+// todo:屎一样的代码, 后面优化
 import React from 'react';
 
 import {
@@ -9,12 +10,12 @@ import {
     StyleSheet,
     Clipboard,
     CameraRoll,
-    ListView
 } from 'react-native';
 import RNFS from "react-native-fs";
 import Toast from 'react-native-easy-toast'
 import ActionButton from 'react-native-action-button';
 import Config from '../config'
+
 const readUrl = {};
 
 export default class HomeScreen extends React.Component {
@@ -23,8 +24,57 @@ export default class HomeScreen extends React.Component {
     this._onEndEditing = this._onEndEditing.bind(this);
     this.state = {
       showValue:"",
+        m3u8videoData:[]
     };
   }
+
+  downloadts(title, url, headers, index, videoData){
+        var fileName = `${title.replace("：", "")}_${index}.ts`;
+        const downloadDest = `${Config.download_path}/${fileName}`;
+        var options = {
+                headers:headers,
+                fromUrl: url,
+                toFile: downloadDest,
+                background: true,
+            };
+        const ret = RNFS.downloadFile(options);
+        ret.promise.then(resp => {
+            videoData[index] = downloadDest;
+            if (videoData.filter(filename => {return filename != undefined}).length == videoData.length){ // 完成， 合并文件
+                var arrayLength = videoData.length;
+                videoData.map(filename => {
+                    RNFS.readFile(filename, "ascii").then(content => {
+                        RNFS.appendFile(`${Config.download_path}/${title.replace("：", "")}.ts`, content, "ascii").then(()=>{
+                            RNFS.unlink(filename).then(()=>{
+                                arrayLength--;
+                                if (arrayLength == 0) { // 完成
+                                    CameraRoll.saveToCameraRoll(`${Config.download_path}/${title.replace("：", "")}.ts`, "video")
+                                        .then(_ => {Alert.alert("下载完成", `[${title.replace("：", "")}.ts]已保存至相册`)})
+                                        .catch(err =>{console.warn(err)});
+                                }
+                            }).catch(error =>{console.error(error)})
+                        }).catch(error =>{console.error(error)})
+                    }).catch(error =>{console.error(error)})
+                })
+            }
+            }).catch(err => {
+            if (err.toString().match("such file") != null){
+              Alert.alert("客户端错误", "请在设置->授权管理->应用权限管理中打开'读写手机存储'权限后重试！" + downloadDest)
+            }else {
+                console.error(err)
+            }
+          });
+  }
+
+  DownloadM3u8(data) {
+    var title = "";
+    var videoData = new Array(data.data.info.length);
+    Alert.alert('下载开始', `正在下载、合并文件，请勿再次点击下载`);
+    for (var i = 0; i < data.data.info.length; i++){
+        title = data.data.info[i].title;
+        this.downloadts(title, unescape(data.data.info[i].url), data.data.headers, i, videoData);
+    }
+}
 
   download(options, downloadDest, fileName){
         try {
@@ -121,10 +171,35 @@ export default class HomeScreen extends React.Component {
           }else{
             this.refs.toast.show(`总计${data.data.info.length}个文件，开始下载`);
             for (var i = 0; i < data.data.info.length; i++){
+                if (data.data.info[i].type == "m3u8"){
+                    RNFS.exists(`${Config.download_path}/${data.data.info[i].title.replace("：", "")}.ts`).then(isExists => {
+                        if (isExists){
+                            Alert.alert("文件已存在", `文件${data.data.info[i].title.replace("：", "")}.ts已存在本地,是否覆盖?`, [
+                                {
+                                    text: "覆盖",
+                                    onPress: ()=>{
+                                        RNFS.unlink(`${Config.download_path}/${data.data.info[i].title.replace("：", "")}.ts`).then(()=>{ //先删除
+                                            this.DownloadM3u8(data);
+                                        });
+                                    }
+                                }, {
+                                    text: "取消下载",
+                                    onPress: ()=>{
+                                        this.refs.toast.show(`已取消下载`, 3000);
+                                    }
+                                }
+                            ])
+                        }else{
+                            this.DownloadM3u8(data);
+                        }
+                    });
+                    break
+                }
               this.downloadFile(data.data.info[i].url, data.data.info[i].type, data.data.headers, data.data.info[i].title, i);
             }
           }
         }).catch(error => {
+            console.error(error);
           Alert.alert("客户端错误", "检查服务器配置以及本地网络连接！")
         }),
     new Promise(function(resolve,reject){
@@ -136,6 +211,7 @@ export default class HomeScreen extends React.Component {
         //请求失败
     });
   }
+
     async _setClipboardContent(){
         try {
             var content = await Clipboard.getString();
